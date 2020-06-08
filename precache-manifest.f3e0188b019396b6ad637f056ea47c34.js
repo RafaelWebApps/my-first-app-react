6bb7a93c6fcd9e7a3455a8f1920afb9df81f5593
RafaelWebApps <rafael.websistemas@gmail.com>
2020-06-08 20:11:10 -0300

    First commit

---
+++ precache-manifest.f3e0188b019396b6ad637f056ea47c34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3359e411c061730c34cfd5349046e3f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3598b5df33ff6fd5c9dc",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "863cf6ad8a1c0cb6e4e0",
+    "url": "/static/js/2.750e34ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.750e34ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3598b5df33ff6fd5c9dc",
+    "url": "/static/js/main.3869da1a.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  }
+]);
